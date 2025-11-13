609dfe15bd4151f14e5a36606c5281898f8487bb
umer1157 <umerzubair3346@gmail.com>
2025-11-13 18:53:24 +0500

    First Commit 18.0

--- router.js
+++ router.js
@@ -10,15 +10,14 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
+
   next();
 });
 
